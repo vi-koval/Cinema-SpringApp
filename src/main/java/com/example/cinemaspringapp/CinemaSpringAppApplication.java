b1464a884f7ad411a9872394055ca70dffeebbf9
DirtyUgly <46310185+DirtyUgly@users.noreply.github.com>
2021-07-29 14:05:56 +0300

    Initial commit

---
+++ src/main/java/com/example/cinemaspringapp/CinemaSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemaspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cinemaspringapp/CinemaSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemaspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
