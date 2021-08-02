f2a3d69d111e25e8c54f89ebf48602ca2c15a04c
DirtyUgly <46310185+DirtyUgly@users.noreply.github.com>
2021-08-02 18:45:39 +0300

    0.2

--- src/main/java/com/example/cinemaspringapp/messages/Messages.java
+++ src/main/java/com/example/cinemaspringapp/messages/Messages.java
@@ -2,8 +2,8 @@ package com.example.cinemaspringapp.messages;
 
 public class Messages {
     public final static String USER_NOT_FOUND_MSG = "User with email %s not found";
-    public final static String USER_ID_NOT_FOUND_MSG = "User with id %d not found";
+    //public final static String USER_ID_NOT_FOUND_MSG = "User with id %d not found";
     public final static String USER_ALREADY_EXISTS_MSG = "User with email %s already exists";
 
-    public final static String CREATING_MULTIPLE_ORDERS = "Creating multiple orders";
+    //public final static String CREATING_MULTIPLE_ORDERS = "Creating multiple orders";
 }
