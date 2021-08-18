4a65387679b9eb65452e0f04f732dd88f10ab7b0
DirtyUgly <46310185+DirtyUgly@users.noreply.github.com>
2021-08-18 21:19:08 +0300

    1.1.1

--- src/main/java/com/example/cinemaspringapp/controller/AdminController.java
+++ src/main/java/com/example/cinemaspringapp/controller/AdminController.java
@@ -25,69 +25,69 @@ import java.util.Optional;
 @RequestMapping("user/admin")
 public class AdminController {
 
-   private final OrderService orderService;
-   private final MovieService movieService;
+    private final OrderService orderService;
+    private final MovieService movieService;
 
-   @Autowired
+    @Autowired
     public AdminController(OrderService orderService,
                            MovieService movieService) {
-       this.orderService = orderService;
-       this.movieService = movieService;
+        this.orderService = orderService;
+        this.movieService = movieService;
 
-   }
+    }
 
-   @GetMapping("/all-orders")
-   public String getAllOrders(@RequestParam("page") Optional<Integer> pageNo,
-                              @RequestParam("sortBy") Optional<String> sortBy,
-                              @RequestParam("direction") Optional<String> direction,
-                              Model model) {
+    @GetMapping("/all-orders")
+    public String getAllOrders(@RequestParam("page") Optional<Integer> pageNo,
+                               @RequestParam("sortBy") Optional<String> sortBy,
+                               @RequestParam("direction") Optional<String> direction,
+                               Model model) {
 
-       int currentPage = pageNo.orElse(1);
-       String sort = sortBy.orElse("id");
-       String dir = "asc".equalsIgnoreCase(direction.orElse("asc")) ? "asc" : "desc";
+        int currentPage = pageNo.orElse(1);
+        String sort = sortBy.orElse("id");
+        String dir = "asc".equalsIgnoreCase(direction.orElse("asc")) ? "asc" : "desc";
 
-       Page<Order> page = orderService.getPaginated(currentPage, sort, dir);
-       List<Order> orders = page.getContent();
+        Page<Order> page = orderService.getPaginated(currentPage, sort, dir);
+        List<Order> orders = page.getContent();
 
-       model.addAttribute("orders", orders);
-       model.addAttribute("totalPages", page.getTotalPages());
-       model.addAttribute("totalItems", page.getTotalElements());
-       model.addAttribute("currentPage", currentPage);
-       model.addAttribute("sortField", sort);
-       model.addAttribute("direction", dir);
-       model.addAttribute("reverseDirection", dir.equals("asc") ? "desc" : "asc");
+        model.addAttribute("orders", orders);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("sortField", sort);
+        model.addAttribute("direction", dir);
+        model.addAttribute("reverseDirection", dir.equals("asc") ? "desc" : "asc");
 
-       return "user/admin/orders";
-   }
+        return "user/admin/orders";
+    }
 
-   @GetMapping("/movies")
-   public String listMovies (@RequestParam("page") Optional<Integer> pageNo,
+    @GetMapping("/movies")
+    public String listMovies(@RequestParam("page") Optional<Integer> pageNo,
                              @RequestParam("sortBy") Optional<String> sortBy,
                              @RequestParam("direction") Optional<String> direction,
-                             Model model){
+                             Model model) {
 
-       int currentPage = pageNo.orElse(1);
+        int currentPage = pageNo.orElse(1);
 
-       String sort = sortBy.orElse("id");
-       String dir = "asc".equalsIgnoreCase(direction.orElse("asc")) ? "asc" : "desc";
+        String sort = sortBy.orElse("id");
+        String dir = "asc".equalsIgnoreCase(direction.orElse("asc")) ? "asc" : "desc";
 
-       Page<Movie> page = movieService.getPaginatedMovie(currentPage, sort, dir);
-       List<Movie> movies = page.getContent();
+        Page<Movie> page = movieService.getPaginatedMovie(currentPage, sort, dir);
+        List<Movie> movies = page.getContent();
 
-       model.addAttribute("movies", movies);
-       model.addAttribute("totalPages", page.getTotalPages());
-       model.addAttribute("totalItems", page.getTotalElements());
-       model.addAttribute("currentPage", currentPage);
-       model.addAttribute("sortField", sort);
-       model.addAttribute("direction", dir);
-       model.addAttribute("reverseDirection", dir.equals("asc") ? "desc" : "asc");
+        model.addAttribute("movies", movies);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("sortField", sort);
+        model.addAttribute("direction", dir);
+        model.addAttribute("reverseDirection", dir.equals("asc") ? "desc" : "asc");
 
-       return "user/admin/movies";
-   }
+        return "user/admin/movies";
+    }
 
 
     @GetMapping("movies/new")
-    public String createNewMovie(@ModelAttribute ("movie")MovieDTO movieDTO, Model model) {
+    public String createNewMovie(@ModelAttribute("movie") MovieDTO movieDTO, Model model) {
         model.addAttribute("exception", "");
         return "user/admin/create-movie";
 
@@ -105,36 +105,46 @@ public class AdminController {
             return "/error";
         }
 
-        try {
-            movieService.saveMovie(movieDTO);
-        } catch (MovieException e) {
-            model.addAttribute("exception", movieDTO.getMovieName());
-            return "user/admin/movies";
+            try {
+                movieService.saveMovie(movieDTO);
+            } catch (MovieException e) {
+                model.addAttribute("exception", movieDTO.getMovieName());
+                return "user/admin/movies";
+            }
+            return "redirect:/user/admin/movies";
         }
-        return "redirect:/user/admin/movies";
+
+
+        @GetMapping("movies/delete")
+        public String deleteMovie ( @RequestParam("id") long id){
+            movieService.deleteMovieById(id);
+            return "redirect:/user/admin/movies";
+        }
+
+
+        @GetMapping("/movies/edit/{id}")
+        public String editMovieForm (@PathVariable Long id, Model model) {
+            model.addAttribute("movie", movieService.getMovieById(id));
+            return "/user/admin/edit-movie";
+        }
+
+
+        @PostMapping("/movies/{id}")
+        public String editMovie (@PathVariable Long id,
+                @ModelAttribute("movie") Movie movie, Model model){
+
+            Movie existingMovie = (Movie) movieService.getMovieById(id);
+
+            existingMovie.setId(id);
+            existingMovie.setMovieName(movie.getMovieName());
+            existingMovie.setDirector(movie.getDirector());
+            existingMovie.setCountry(movie.getCountry());
+            existingMovie.setYear(movie.getYear());
+
+
+            movieService.editMovie(existingMovie);
+            return "redirect:/user/admin/movies";
+
+        }
     }
 
-@GetMapping("movies/delete")
-   public String deleteMovie(@RequestParam("id") long id){
-       movieService.deleteMovieById(id);
-       return "redirect:/user/admin/movies";
-   }
-
-   @GetMapping("/movies/edit/{id}")
-    public String editMovieForm (@PathVariable Long id, Model model) {
-    model.addAttribute("movie", movieService.getMovieById(id));
-    return "/user/admin/edit-movie";
-   }
-
-@PostMapping("/movies/{id}")
-   public String editMovie (@PathVariable Long id,
-                            @ModelAttribute ("movie") Movie movie, Model model) {
-
-       Movie existingMovie = movieService.getMovieById(id);
-       existingMovie.setMovieName(movie.getMovieName());
-
-
-}
-
-
-}
